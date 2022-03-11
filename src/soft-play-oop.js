209f6bb616da75b9687cdd22d3788d28698b676d
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-03-11 12:09:56 +0000

    soft-play-oop.spec.js test pass

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,6 +1,50 @@
 // TODO: Write your class in this file
+class softPlay {
+  constructor() {
+    this.adults = 0;
+    this.children = 0;
+  }
+  occupancy() {
+    return {
+      adults: this.adults,
+      children: this.children,
+    };
+  }
 
+  enter(numAdults, numChildren) {
+    const isSupervised = numAdults;
+    if (isSupervised) {
+      this.adults += numAdults;
+      this.children += numChildren;
+      return true;
+    }
+    else return false;
+  }
+
+  // leave () {
+  //   this.adults -= 0
+  //   this.children -= 1
+  // }
+
+  leave(numAdults, numChildren) {
+    const isSupervised = numAdults;
+    if (numAdults > this.adults || numChildren > this.children) {
+      return false;
+    }
+    else if (this.adults - numAdults === 0 && this.children - numChildren > 0) {
+      return false;
+    }
+    else if (this.children - numChildren === 0 && this.adults - numAdults > 0) {
+      return false;
+    }
+    else if (isSupervised) {
+      this.adults -= numAdults;
+      this.children -= numChildren;
+      return true
+    }
+  }
+}
 // TODO: Change undefined to the name of your class
 module.exports = {
-  SoftPlay: undefined
-}
+  SoftPlay: softPlay,
+};
